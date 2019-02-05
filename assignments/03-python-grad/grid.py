53342724fd37b660aee314afe6c1c9a1a8770153
Wen Yu Wong <wwong3@login3.cm.cluster>
2019-02-05 12:40:45 -0700

    hw 03-grad completed#

--- assignments/03-python-grad/grid.py
+++ assignments/03-python-grad/grid.py
@@ -26,20 +26,20 @@ def main():
 
 # First if statement: Error if num is not between 2 and 9
 # Elif statement: If num is between 2 and 9, will display grid 
-    num=int(num[0])
+    num=int(num[0]) #changes num from a list to an integer
     if  not 2<= num <10:
         print('NUM ({}) must be between 1 and 9'.format(num))
         sys.exit(1)
     elif 1 < num < 10:
-       grid = []
-       row=[]
-       column=[]
-       last_num=num+1
-       total_range=num*num
-       for j in range (1,last_num): # iterate through cols
-           for i in range(1,last_num): # iterate through rows
-               print('{:>2d}'.format(i+num*(j-1)),end=' ')
-           print('')
+        last_num=num+1 # to include last number through indexing
+        for j in range (1,last_num): # iterate through cols
+            for i in range(1,last_num): # iterate through rows         
+                print('{:>3d}'.format(i+num*(j-1)),end='')
+                # i+num*(j-1) is pattern for grid output
+                # don't use {:>2d) with end=' ' > will give you extra space at end of line
+                #{:>3d} is string formatting
+                 # 3=num of character field, d=decimal, > means right-aligned
+            print('') #print new line
           
-       exit(0)
+        exit(0)
 main()
