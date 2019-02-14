a43068e0419940a1f545fb3666dc5aec38862f2a
Wen Yu Wong <wwong3@login2.cm.cluster>
2019-02-13 23:38:10 -0700

    hwk 4 grad completed

--- assignments/04-python-tictactoe/tictactoe.py
+++ assignments/04-python-tictactoe/tictactoe.py
@@ -40,6 +40,9 @@ def main():
 	player_arg = args.player
 	cell_arg = args.cell	
 
+	# Converting '.' input to '-' output for good state, mutate state and other mutate state 
+	state_arg=state_arg.replace('.', '-')
+
 # Determining whether there is a bad state argument
 	# counting for -XO
 	ok_state={i:0 for i in '-XO'}
@@ -50,6 +53,7 @@ def main():
 
 	# Determining coniditions when state dies
 	if len(state_arg) !=9 or count !=9:
+		state_arg=state_arg.replace('-', '.') # Switching back from - to . so print works
 		print('Invalid state "{}", must be 9 characters of only -, X, O'.format(state_arg))
 		sys.exit(1)
 						
@@ -71,30 +75,39 @@ def main():
 		if (not player_arg) or (not cell_arg):
 			print('Must provide both --player and --cell')
 			sys.exit(1)	
+	
+# From a given state, change one cell to the player's choice
+	if len (player_arg) !=0 and cell_arg !=None:
+		state_list=list(state_arg) # Changing state_arg into a list
+		if state_list[cell_arg-1] in 'XO': # Calling for cell_arg-1 that might have 'XO'
+			print('Cell {} already taken'.format(cell_arg))
+			sys.exit(1)
+		else:
+			state_list[cell_arg-1]=player_arg # Setting the cell to the player's choice
+			new_state=(''.join(state_list))  # Joining together new state_list
+			state_arg=new_state # Updating state_arg to the new inputs 
+ 
 
-
-	board()
+	board(state_arg) # Calling in the new status of state_arg
 
 
 # --------------------------------------------------
-def board():
-	cell=[1,2,3,4,5,6,7,8,9]
-	# assign values to the different cells
-	square=['| 1 | 2 | 3 |\n','| 4 | 5 | 6 |\n','| 7 | 8 | 9 |\n']
-	
-	# print cells as a string type with the border 
-	
-	print('-------------\n'.join(['']+square+['']))
+def board(cell):
+	new_board=[]
+	for i, char in enumerate(cell):
+		if char == '-': # If your state board has a -, then replace the - with the cell value
+			char=i+1 # i+1 since cell is based on 1 while char is based on 0
+		new_board.append(char)
+		
+	# print cells as a string type with the border
+	print('-------------')
+	print('| {} | {} | {} |'.format(new_board[0], new_board[1], new_board[2]))
+	print('-------------')
+	print('| {} | {} | {} |'.format(new_board[3], new_board[4], new_board[5]))
+	print('-------------')
+	print('| {} | {} | {} |'.format(new_board[6], new_board[7], new_board[8])) 
+	print('-------------')
 
-# --------------------------------------------------
-# def bad_state(args):
-#	state_arg=args.state	
-#	if len(state_arg) != 9:
-#		print('"{}" must be 9 characters of only -, X, O'.format(state_arg))
-#		sys.exit(1)
-#	if state_arg !=state_arg.choices
-#		print('"{}" must be 9 characters of only -, X, O'.format(state_arg))
-#		sys.exit(1)
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
