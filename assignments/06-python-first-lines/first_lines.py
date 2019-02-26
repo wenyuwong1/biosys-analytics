b80195ebe302b6c202e6ffd1376c344175aa73cf
Wen Yu Wong <wwong3@login2.cm.cluster>
2019-02-25 23:37:04 -0700

    finished

--- assignments/06-python-first-lines/first_lines.py
+++ assignments/06-python-first-lines/first_lines.py
@@ -17,13 +17,14 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument(
-        'positional', metavar='DIR', help='Directory name', nargs='+')
+        'directories', metavar='DIR', help='Directory name', nargs='+')
 
     parser.add_argument(
         '-w',
         help='width',
         metavar='int',
-        type=int)
+        type=int,
+	default=50)
 
     return parser.parse_args()
 
@@ -45,16 +46,30 @@ def die(msg='Something bad happened'):
 def main():
 	"""Make a jazz noise here"""
 	args = get_args()
-	dir=args.positional
+	dirs=args.directories
 	width=args.w
-
-	# Error msg if input is not a directory
-	dir.verifyIsDirectory()
-	print('{} is not a file'.format(dir))
-	exit(1)
 	
+	# Determine if positional value is a directory
+	for dir in dirs:
+		if not os.path.isdir(dir):
+			print('"{}" is not a directory'.format(dir), file=sys.stderr)
+			continue
+		dir_dict={} # create empty to store keys and f_line later
+		print(dir)
+		files = os.listdir(dir)
+		for file in files:
+			path=os.path.join(dir,file) # path relative to script location
+			with open(path) as f:
+				f_line=f.readline().rstrip() # read first line and strip /n
+				dir_dict[f_line]=file
+		dir_sort=sorted(dir_dict.keys())
+		for f_line in dir_sort:
+			file=dir_dict[f_line]
+			dots='.'*(width-len(f_line)-len(file)) # figure out num of dots needed
+			print('{} {} {}'.format(f_line, dots, dir_dict[f_line])) 
+
 
 
 # --------------------------------------------------
 if __name__ == '__main__':
-    main()
+	main()
