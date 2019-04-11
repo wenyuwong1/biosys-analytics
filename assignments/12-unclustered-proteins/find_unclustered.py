3c1d54a6d2d6b99540e4d0eb53ae6606f39c104c
Wen Yu Wong <wwong3@login3.cm.cluster>
2019-04-10 23:22:22 -0700

    add find_unclustered

--- assignments/12-unclustered-proteins/find_unclustered.py
+++ assignments/12-unclustered-proteins/find_unclustered.py
@@ -8,6 +8,8 @@ Purpose: Unclustered Proteins
 import argparse
 import sys
 import os
+import re
+from Bio import SeqIO
 
 # --------------------------------------------------
 def get_args():
@@ -60,18 +62,49 @@ def die(msg='Something bad happened'):
 
 # --------------------------------------------------
 def main():
-	"""Make a jazz noise here"""
-	args = get_args()
-	cdhit=args.cdhit
-	proteins=args.proteins
-	outfile=args.outfile
+    """Make a jazz noise here"""
+    args = get_args()
+    cdhit = args.cdhit
+    proteins = args.proteins
+    outfile = args.outfile
 
-	if not os.path.isfile(proteins):
-		die('--proteins "{}" is not a file'.format(proteins))
-	if not os.path.isfile(cdhit):
-		die('--cdhit "{}" is not a file'.format(cdhit))
+    if not os.path.isfile(proteins):
+        die('--proteins "{}" is not a file'.format(proteins))
+    if not os.path.isfile(cdhit):
+        die('--cdhit "{}" is not a file'.format(cdhit))
 
-		
+    cdhit_match = re.compile('(>gi)'
+                             '[|]'
+                             '(?P<PID>\d+)'
+                             '[|]')
+
+    clustered = set()
+
+    with open(cdhit) as hit_handle:
+        for line in hit_handle:
+            m1 = cdhit_match.search(line)
+            if m1==None:
+                continue
+            clustered.add(m1.group('PID'))
+
+    unclustered = 0
+    num_proteins = 0
+
+    out_file=open(outfile, 'wt')
+    with open(proteins) as protein_handle:
+        for record in SeqIO.parse(protein_handle, 'fasta'):
+            num_proteins+=1
+            m2=re.search('(?P<ID>\d+)', record.id) #record.id drops the >
+            if m2==None:
+                continue
+            ID=m2.group('ID')
+
+            if ID not in clustered:
+                unclustered+=1
+                SeqIO.write(record, out_file, "fasta")
+    print('Wrote {:,} of {:,} unclustered proteins to "{}"'.format(unclustered, num_proteins, outfile))
+
+
 
 
 # --------------------------------------------------
